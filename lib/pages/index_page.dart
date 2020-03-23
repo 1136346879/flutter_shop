1918bf5282b121eac6c9dedc917a129a40320ea5
张研 <1136346879@qq.com>
2020-03-23 16:40:39 +0800

    app的UI框架搭建

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
+
+class IndexPage extends StatefulWidget {//动态切换页面 用 StatefulWidget
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text("书籍")),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.car), title: Text("汽车")),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.shopping_cart), title: Text("购物车")),
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.book), title: Text("会员")),
+  ];
+
+  final List pageList = [HomePage(), CategoryPage(), CartPage(), MemberPage()];
+
+  int currentIndex = 0;
+  var currentPage;
+  @override
+  void initState() {
+    currentPage = pageList[currentIndex];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(245, 245, 245, 1.0),//北京
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,//类型
+        items: bottomTabs,//底部tab
+        currentIndex: currentIndex,//当前 index
+        onTap: (index) {//点击后调用 setState 方法  然后会重新绘制页面
+          setState(() {
+            currentIndex = index;//点击的tab
+            currentPage = pageList[currentIndex];//当前页面
+          });
+        },
+      ),
+      body: currentPage,//主体显示的是当前的页面
+    );
+  }
+}
